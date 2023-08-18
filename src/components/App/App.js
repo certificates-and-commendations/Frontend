16bb5272614d769830f45c348f281947ec68a74e
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-08-18 17:41:12 +0300

    feat(app): добавил ProtectedRoute + базовый роутинг

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,36 @@
 import './App.css';
+// import { useState } from 'react';
+import { Routes } from 'react-router-dom';
+
+// import { Route, Navigate } from 'react-router-dom';
+
+// import ProtectedRouteElement from '../ProtectedRoute/ProtectedRoute';
 
 function App() {
+	// СТЕЙТ СОСТОЯНИЯ LOGIN
+	// const [isloggedIn, setIsloggedIn] = useState(false);
+
 	return (
-		<div className="App">
-			<p>Hello again!</p>
-			<p>Hello again2!</p>
-			<p>Hello again3!</p>
-			<p>Hello again4!</p>
-		</div>
+		<Routes >
+			{/* По готовности компонента Main кладем его в роут */}
+			{/* <Route path='/' element={ Ожидаю Main } /> */}
+
+			{/* По готовности компонента Main кладем его в роут */}
+			{/* <Route path='/signup' element={isloggedIn ? <Navigate to='/' /> : Ожидаю SignUp />} /> */}
+
+			{/* По готовности компонента Main кладем его в роут */}
+			{/* <Route path='/signin' element={isloggedIn ? <Navigate to='/' /> : Ожидаю SignIn />} /> */}
+
+			{/* По готовности компонента Editor кладем его в роут */}
+			{/* <Route path='/editor' element={<ProtectedRouteElement loggedIn={isloggedIn} element={ Ожидаю Editor } />} /> */}
+
+			{/* По готовности компонента Editor кладем его в роут */}
+			{/* <Route path='/profile' element={<ProtectedRouteElement loggedIn={isloggedIn} element={ Ожидаю Profile } />} /> */}
+
+			{/* По готовности компонента NotFoundPage кладем его в роут */}
+			{/* <Route path='*' element={ Ожидаю NotFoundPage } /> */}
+
+		</Routes>
 	);
 }
 
