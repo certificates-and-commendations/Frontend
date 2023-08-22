57e03f2a41afc3aa1d69704ad2f7e4fc6bceb8e5
govard9 <web-supsu@yandex.ru>
2023-08-22 23:28:17 +0700

    fix: test

--- src/components/CertificateUploader/CertificateUploader.jsx
+++ src/components/CertificateUploader/CertificateUploader.jsx
@@ -1,43 +1,42 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-function CertificateUploader({ onUpload }) {
-	const [uploadedImage, setUploadedImage] = useState(null);
+function CertificateUploader({onUpload}) {
+    const [uploadedImage, setUploadedImage] = useState(null);
 
-	const handleImageUpload = (e) => {
-		const file = e.target.files[0];
-		if (file) {
-			if (file.type === 'image/jpeg' || file.type === 'image/png') {
-				const reader = new FileReader();
-				reader.onload = (event) => {
-					setUploadedImage(event.target.result);
-					onUpload(event.target.result);
-				};
-				reader.readAsDataURL(file);
-			} else {
-				setUploadedImage(null);
-				alert('Пожалуйста, загрузите изображение в формате JPEG или PNG.');
-			}
-		}
-	};
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            if (file.type === 'image/jpeg' || file.type === 'image/png') {
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    setUploadedImage(event.target.result);
+                    onUpload(event.target.result);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                setUploadedImage(null);
+                alert('Пожалуйста, загрузите изображение в формате JPEG или PNG.');
+            }
+        }
+    };
 
-	return (
-		<div className="certificate-uploader">
-
-			<label className="certificate-uploader__label" htmlFor="imageUpload">
+    return (
+        <div className="certificate-uploader">
+            <label className="certificate-uploader__label" htmlFor="imageUpload">
 				<span className="certificate-uploader__span">
 					Загрузите свою грамоту:
 				</span>
-			</label>
-			<input
-				type="file"
-				accept="image/jpeg,image/png"
-				onChange={handleImageUpload}
-				id="imageUpload"
-				className="certificate-uploader__input"
-			/>
-			{uploadedImage && <img src={uploadedImage} alt="Uploaded Certificate" />}
-		</div>
-	);
+                <input
+                    id="imageUpload"
+                    type="file"
+                    accept="image/jpeg,image/png"
+                    onChange={handleImageUpload}
+                    className="certificate-uploader__input"
+                />
+            </label>
+            {uploadedImage && <img src={uploadedImage} alt="Uploaded Certificate"/>}
+        </div>
+    );
 }
 
 export default CertificateUploader;
