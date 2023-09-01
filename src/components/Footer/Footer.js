400a4fd9cd94191e99377114d779f4f188aac5be
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-09-01 14:50:35 +0300

    feat: написал footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import Logo from '../Logo/Logo';
+import './Footer.css';
+import Telegram from '../../images/Telegram - Negative.svg';
+
+export default function Footer() {
+	return (
+		<footer className="footer">
+			<div className="footer__column">
+				<Logo />
+				<div className="footer__info">
+					<p className="footer__text footer__text_margin_bottom">
+						© 2023 IT сообщество
+					</p>
+					<p className="footer__text">Напишите нам</p>
+					<p className="footer__text">Обратная связь и предложения</p>
+				</div>
+			</div>
+			<a className="footer__link" href="p" target="_blank">
+				<img
+					className="footer__link-img"
+					src={Telegram}
+					alt="Логотип телеграмма"
+				/>
+			</a>
+		</footer>
+	);
+}
