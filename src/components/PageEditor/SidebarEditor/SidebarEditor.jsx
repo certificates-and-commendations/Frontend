d8ced64aab6e147db70adc849e0cfb8b369d85e0
govard9 <web-supsu@yandex.ru>
2023-09-07 09:23:07 +0700

    feat: создать панель Текст в сайдбаре

--- src/components/PageEditor/SidebarEditor/SidebarEditor.jsx
+++ src/components/PageEditor/SidebarEditor/SidebarEditor.jsx
@@ -3,38 +3,43 @@ import templateImage from '../../../images/imageEditor/templateImage.svg'
 import textImage from '../../../images/imageEditor/textImage.svg'
 import elementImage from '../../../images/imageEditor/elementImage.svg'
 import downloadImage from '../../../images/imageEditor/downloadImage.svg'
+import TemplatesPanel from "./TemplatesPanel/TemplatesPanel";
+import TextPanel from "./TextPanel/TextPanel";
 
 function SidebarEditor(props) {
     return (
         <aside className="sidebar">
             <nav className="sidebar-navigate">
                 <ul className="sidebar-navigate__items">
-                    <li className="sidebar-navigate__item sidebar-navigate__item_active">
+                    <li className="sidebar-navigate__item">
                         <button className="sidebar-navigate__button">
-                            <img src={templateImage} alt="" className="sidebar-navigate__icon"/>
+                            <img src={templateImage} alt=" Кнопка для открытия панели с шаблонами." className="sidebar-navigate__icon"/>
                             Шаблоны
                         </button>
                     </li>
-                    <li className="sidebar-navigate__item">
+                    <li className="sidebar-navigate__item sidebar-navigate__item_active">
                         <button className="sidebar-navigate__button">
-                            <img src={textImage} alt="" className="sidebar-navigate__icon"/>
+                            <img src={textImage} alt=" Кнопка для открытия панели для создания текста." className="sidebar-navigate__icon"/>
                             Текст
                         </button>
                     </li>
                     <li className="sidebar-navigate__item">
                         <button className="sidebar-navigate__button">
-                            <img src={elementImage} alt="" className="sidebar-navigate__icon"/>
+                            <img src={elementImage} alt=" Кнопка для открытия панели с элементами." className="sidebar-navigate__icon"/>
                             Элементы
                         </button>
                     </li>
                     <li className="sidebar-navigate__item">
                         <button className="sidebar-navigate__button">
-                            <img src={downloadImage} alt="" className="sidebar-navigate__icon"/>
+                            <img src={downloadImage} alt=" Кнопка открытия панели загрузок." className="sidebar-navigate__icon"/>
                             Загрузки
                         </button>
                     </li>
                 </ul>
             </nav>
+            <section className="panel-container">
+                <TextPanel />
+            </section>
         </aside>
     );
 }
--- src/components/PageEditor/SidebarEditor/TextPanel/TextPanel.jsx
+++ src/components/PageEditor/SidebarEditor/TextPanel/TextPanel.jsx
@@ -1,44 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import downloadIcon from '../../../../images/imageEditor/download-icon.png';
 
 function TextPanel(props) {
-
-    const [btnClick, setBtnClick] = useState(true);
-
-    const onClickBtnActive = () => {
-        setBtnClick(false);
-    }
-
-    const onClickBtnNotActive = () => {
-        setBtnClick(true);
-    }
-
     return (
         <div className="text-panel">
             <div className="text-panel__block-button">
-                <button
-                    className={`text-panel__button ${btnClick ? "text-panel__button_active" : ""}`}
-                    onClick={onClickBtnNotActive}>
-                    Текст
-                </button>
-                <button
-                    className={`text-panel__button ${!btnClick ? "text-panel__button_active" : ""}`}
-                    onClick={onClickBtnActive}>Мои шрифты</button>
+                <button className="text-panel_button">Текст</button>
+                <button className="text-panel_button">Мои шрифты</button>
             </div>
-            { btnClick ?
-                <div className="text-panel__block-result">
-                    <button className="text-panel__h1">Создать заголовок</button>
-                    <button className="text-panel__h2">Создать подзаголовок</button>
-                    <button className="text-panel__paragraph">Создать основной текст</button>
-                </div>
-                :
-                <div className="text-panel__block-download">
-                    <button className="text-panel__download-font">
-                        <img src={downloadIcon} alt="" className="text-panel__download-icon"/>
-                        Загрузить шрифт
-                    </button>
-                </div>
-            }
+            <div className="text-panel__block-result">
+                <button className="text-panel__h1">Создать заголовок</button>
+                <button className="text-panel__h2">Создать заголовок</button>
+                <button className="text-panel__paragraph">Создать заголовок</button>
+            </div>
+            <div className="text-panel__block-result">
+                <button className="text-panel__download-font">
+                    <img src={downloadIcon} alt="" className="text-panel__download-icon"/>
+                    Создать заголовок
+                </button>
+            </div>
         </div>
     );
 }
