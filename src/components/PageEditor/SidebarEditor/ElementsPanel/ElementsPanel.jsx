0296d59b48469e5394ef673629b9d466a19f10c8
govard9 <web-supsu@yandex.ru>
2023-09-10 17:53:26 +0700

    feat: Создать просмотр загруженных элментов и сделать возможность их выбрать галочкой.

--- src/components/PageEditor/SidebarEditor/ElementsPanel/ElementsPanel.jsx
+++ src/components/PageEditor/SidebarEditor/ElementsPanel/ElementsPanel.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import square from '../../../../images/imageEditor/elements-panel__square.svg';
+import squareCheck from '../../../../images/imageEditor/elements-panel__square-check.svg';
 
 function ElementsPanel() {
+
+    const [imageURLs, setImageURLs] = useState([]);
+    const [squareStates, setSquareStates] = useState([]);
+
+    const handleFileInputChange = (e) => {
+        const files = Array.from(e.target.files);
+
+        setImageURLs((prevImageURLs) => [
+            ...prevImageURLs,
+            ...files.map((file) => URL.createObjectURL(file)),
+        ]);
+        setSquareStates((prevStates) => [...prevStates, false]);
+    };
+
+    const handleClickSquare = (index) => {
+        setSquareStates((prevStates) => {
+            const newStates = [...prevStates];
+            newStates[index] = !newStates[index];
+            return newStates;
+        });
+    };
+
     return (
         <div className="elements-panel">
             <div className="elements-panel__block-download">
@@ -8,7 +32,34 @@ function ElementsPanel() {
                     Вы можете загрузить подпись, печать в формате JPEG/PNG
                     &nbsp;И&nbsp;таблицу&nbsp;Exel&nbsp;(с ФИО если нужно оформить несколько грамот)
                 </p>
-                <button className="elements-panel__btn-download">Загрузить файл</button>
+                <label htmlFor="fileElementsInput" className="elements-panel__btn-download">
+                    Загрузить файл
+                    <input
+                        type="file"
+                        id="fileElementsInput"
+                        className="elements-panel__input"
+                        multiple
+                        onChange={handleFileInputChange}
+                    />
+                </label>
+            </div>
+            <div className="elements-panel__loading-file">
+                {imageURLs.map((url, index) => (
+                    <div className="elements-panel__wrapper">
+                        <img
+                            key={index}
+                            src={url}
+                            alt={`Загруженное изображение ${index}`}
+                            className="elements-panel__loading-img"
+                        />
+                        <img
+                            src={squareStates[index] ? squareCheck : square}
+                            alt={squareStates[index] ? ' Квадрат с галочкой.' : ' Пустой квадрат.'}
+                            className="elements-panel__square"
+                            onClick={() => handleClickSquare(index)}
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
