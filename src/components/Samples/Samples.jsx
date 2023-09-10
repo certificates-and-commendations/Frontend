f102e63940a686656fa368f52d5ca20cdbfac669
OGSmit <og88tuf15@gmail.com>
2023-09-10 14:30:20 +0300

    fast commit

--- src/components/Samples/Samples.jsx
+++ src/components/Samples/Samples.jsx
@@ -10,75 +10,75 @@ import sampleImageHorizontal from '../../images/horizontal2.svg';
 function Samples({ setDiploma }) {
 	// Массив шаблонов
 	const [samples, setSamples] = useState({
-		count: 2,
-		next: null,
-		previous: null,
-		results: [
+		"count": 2,
+		"next": null,
+		"previous": null,
+		"results": [
 			{
-				id: 2,
-				title: 'horizotal template',
-				thumbnail: sampleImageHorizontal,
-				category: null,
-				color: null,
-				is_horizontal: true,
+				"id": 2,
+				"title": "horizotal template",
+				"thumbnail": sampleImageHorizontal,
+				"category": null,
+				"color": null,
+				"is_horizontal": true
 			},
 			{
-				id: 1,
-				title: 'vertical template',
-				thumbnail: sampleImageVertical,
-				category: null,
-				color: null,
-				is_horizontal: false,
+				"id": 1,
+				"title": "vertical template",
+				"thumbnail": sampleImageVertical,
+				"category": null,
+				"color": null,
+				"is_horizontal": false
 			},
 			{
-				id: 3,
-				title: 'horizotal template',
-				thumbnail: sampleImageHorizontal,
-				category: null,
-				color: null,
-				is_horizontal: true,
+				"id": 3,
+				"title": "horizotal template",
+				"thumbnail": sampleImageHorizontal,
+				"category": null,
+				"color": null,
+				"is_horizontal": true
 			},
 			{
-				id: 4,
-				title: 'vertical template',
-				thumbnail: sampleImageVertical,
-				category: null,
-				color: null,
-				is_horizontal: false,
+				"id": 4,
+				"title": "vertical template",
+				"thumbnail": sampleImageVertical,
+				"category": null,
+				"color": null,
+				"is_horizontal": false
 			},
 			{
-				id: 5,
-				title: 'horizotal template',
-				thumbnail: sampleImageHorizontal,
-				category: null,
-				color: null,
-				is_horizontal: true,
+				"id": 5,
+				"title": "horizotal template",
+				"thumbnail": sampleImageHorizontal,
+				"category": null,
+				"color": null,
+				"is_horizontal": true
 			},
 			{
-				id: 6,
-				title: 'vertical template',
-				thumbnail: sampleImageVertical,
-				category: null,
-				color: null,
-				is_horizontal: false,
+				"id": 6,
+				"title": "vertical template",
+				"thumbnail": sampleImageVertical,
+				"category": null,
+				"color": null,
+				"is_horizontal": false
 			},
 			{
-				id: 7,
-				title: 'horizotal template',
-				thumbnail: sampleImageHorizontal,
-				category: null,
-				color: null,
-				is_horizontal: true,
+				"id": 7,
+				"title": "horizotal template",
+				"thumbnail": sampleImageHorizontal,
+				"category": null,
+				"color": null,
+				"is_horizontal": true
 			},
 			{
-				id: 8,
-				title: 'vertical template',
-				thumbnail: sampleImageVertical,
-				category: null,
-				color: null,
-				is_horizontal: false,
-			},
-		],
+				"id": 8,
+				"title": "vertical template",
+				"thumbnail": sampleImageVertical,
+				"category": null,
+				"color": null,
+				"is_horizontal": false
+			}
+		]
 	});
 	const [checkboxValues, setCheckboxValues] = useState({
 		diplomas: false,
@@ -95,22 +95,21 @@ function Samples({ setDiploma }) {
 		});
 	};
 	// ОТПРАВЛЯЕМ ЗАПРОС НА БЭК ДЛЯ ПОЛУЧЕНИЯ ОТФИЛЬТРОВАНЫХ ШАБЛОНОВ
+	
 	async function getFilteredSamples() {
 		try {
-			const samplesFromBack = await authApi.handleFilterSamples(checkboxValues);
+			const samplesFromBack = await authApi.handleFilterSamples(checkboxValues)
 			setSamples(samplesFromBack);
 		} catch (err) {
-			console.log(err);
+			console.log(err)
 		}
 	}
 	// ПРИ ИЗМЕНЕНИИ ОБЬЕКТА НАСТРОЕК checkboxValues ОТПРАВЛЯЕМ ЗАБРОС НА БЭК
-	// И СЕТАПИМ ШАБЛОНЫ К ПОКАЗУ
+	// И СЕТАПИМ ШАБЛОНЫ К ПОКАЗУ 
 	useEffect(() => {
-		const allFalse = Object.values(checkboxValues).every(
-			(value) => value === false
-		);
+		const allFalse = Object.values(checkboxValues).every((value) => value === false);
 		if (!allFalse) {
-			getFilteredSamples();
+			getFilteredSamples()
 		}
 	}, [checkboxValues]);
 
@@ -160,8 +159,9 @@ function Samples({ setDiploma }) {
 				<div className="samples__container">
 					{samples.results.map((item) => {
 						return (
-							<Link key={item.id} className="samples__link" to="/editor">
+							<Link key={item.id} className='samples__link' to='/editor'>
 								<img
+
 									onClick={() => setDiploma(item)}
 									alt={item.title}
 									className="samples__image"
