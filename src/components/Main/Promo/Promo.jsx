0c6e3df0dd9241c70d237406bb731b041a5e7201
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-09-17 13:14:20 +0300

    fix(samples): fix button like position

--- src/components/Main/Promo/Promo.jsx
+++ src/components/Main/Promo/Promo.jsx
@@ -5,28 +5,21 @@ import dipl from '../../../images/before-word-diplomas.svg';
 import sert from '../../../images/after-word-sertificates.svg';
 
 export default function Promo() {
-
-  return (
-    <section className="promo">
-      <h1 className="promo__title">
-        Онлайн-редактор
-        <img src={mac} alt="Шаблон грамоты" className="promo__image" />
-        грамот,<img
-          src={dipl}
-          alt="Шаблон грамоты"
-          className="promo__image"
-        />
-        дипломов и сертификатов
-        <img src={sert} alt="Шаблон грамоты" className="promo__image" />
-      </h1>
-      <Link className="promo__link" to="/editor">
-        <button
-          className="promo__button"
-          type="button"
-        >
-          Онлайн-редактор
-        </button>
-      </Link>
-    </section>
-  );
+	return (
+		<section className="promo">
+			<h1 className="promo__title">
+				Онлайн-редактор
+				<img src={mac} alt="Шаблон грамоты" className="promo__image" />
+				грамот,
+				<img src={dipl} alt="Шаблон грамоты" className="promo__image" />
+				дипломов и сертификатов
+				<img src={sert} alt="Шаблон грамоты" className="promo__image" />
+			</h1>
+			<Link className="promo__link" to="/editor">
+				<button className="promo__button" type="button">
+					Онлайн-редактор
+				</button>
+			</Link>
+		</section>
+	);
 }
