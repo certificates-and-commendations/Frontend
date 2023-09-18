e93367ba3a3af342681f09b1f84c550b88408d3d
govard9 <web-supsu@yandex.ru>
2023-09-18 19:08:42 +0700

    feat: изменить структуру файлов в редакторе. по умолчанию выбрана панель Шаблоны, если нет загрженой грамоты и человек не выбрал из шаблонов, то кнопки в панеле Текст и элементы будут неактивные.

--- src/components/PageEditor/PageEditor.jsx
+++ src/components/PageEditor/PageEditor.jsx
@@ -1,12 +1,255 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import html2canvas from "html2canvas";
+import JsPDF from "jspdf";
 import SidebarEditor from "./SidebarEditor/SidebarEditor";
 import CertificateEditor from "./CertificateEditor/CertificateEditor";
 
-function PageEditor(props) {
+function PageEditor() {
+    const [font, setFont] = useState('Arial');
+    const [fontSize, setFontSize] = useState(14);
+    const [showProperties, setShowProperties] = useState(false);
+    const [textBlocks, setTextBlocks] = useState([]);
+    const [editingTextIndex, setEditingTextIndex] = useState(null);
+    const [signature, setSignature] = useState(null);
+    const [signaturePosition, setSignaturePosition] = useState({x: 0, y: 0});
+    const [uploadedCertificate, setUploadedCertificate] = useState(null);
+    const [showTable, setShowTable] = useState([]);
+    const [tableData, setTableData] = useState([]);
+    const [stamp, setStamp] = useState(null);
+    const [stampPosition, setStampPosition] = useState({x: 0, y: 0});
+    const [textPosition, setTextPosition] = useState({x: 0, y: 0});
+    const [activeTextIndex, setActiveTextIndex] = useState(null);
+    const [textDecorationStyle, setTextDecorationStyle] = useState('none');
+    const [textAlignStyle, setTextAlignStyle] = useState('left');
+    const [pdfData, setPdfData] = useState(null);
+    const [textBlockStyles, setTextBlockStyles] = useState([]);
+    const [textPanelActive, setTextPanelActive] = useState(false);
+
+    const certificateRef = useRef(null);
+
+    const handleTextClick = (e) => {
+        setFontSize(14);
+        setFont('Arial');
+
+        if (!editingTextIndex) {
+            setTextBlocks([
+                ...textBlocks,
+                {
+                    text: '',
+                    x: '',
+                    y: '',
+                    fontFamily: 'Arial',
+                    fontSize: 14,
+                },
+            ]);
+            setTextBlockStyles([
+                ...textBlockStyles,
+                {
+                    isItalic: false,
+                    isBold: false,
+                    isDecoration: 'none',
+                    isAlign: 'left',
+                },
+            ]);
+            setEditingTextIndex(textBlocks.length);
+            setActiveTextIndex(textBlocks.length);
+            setShowProperties(true);
+        }
+    };
+
+    const handleTextChange = (e, index) => {
+        const updatedTextBlocks = [...textBlocks];
+        updatedTextBlocks[index].text = e.target.value;
+        setTextBlocks(updatedTextBlocks);
+    };
+
+    const handleFontChange = (e) => {
+        setFont(e.target.value);
+        if (editingTextIndex !== null) {
+            const updatedTextBlocks = [...textBlocks];
+            updatedTextBlocks[editingTextIndex].fontFamily = e.target.value;
+            setTextBlocks(updatedTextBlocks);
+        }
+    };
+
+    const handleFontSizeChange = (e) => {
+        const updatedTextBlocks = [...textBlocks];
+
+        if (parseInt(e.target.value, 10) > 50) {
+            setFontSize(50);
+            updatedTextBlocks[editingTextIndex].fontSize = 50;
+        } else if (parseInt(e.target.value, 10) < 0) {
+            setFontSize(1);
+            updatedTextBlocks[editingTextIndex].fontSize = 1;
+        } else {
+            setFontSize(parseInt(e.target.value, 10));
+
+            if (editingTextIndex !== null) {
+                updatedTextBlocks[editingTextIndex].fontSize = parseInt(
+                    e.target.value,
+                    10
+                );
+
+                setTextBlocks(updatedTextBlocks);
+            }
+        }
+    };
+
+    const handleInputKeyDown = (e, index) => {
+        if (e.key === 'Enter') {
+            setEditingTextIndex(null);
+            const updatedTextBlocks = [...textBlocks];
+            updatedTextBlocks[index].text = e.target.value;
+            updatedTextBlocks[index].x = textPosition.x;
+            updatedTextBlocks[index].y = textPosition.y;
+            setTextBlocks(updatedTextBlocks);
+            setShowProperties(false);
+            setActiveTextIndex(null);
+        }
+    };
+
+    const handleSignatureUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            if (file.type === 'image/png') {
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    setSignature(event.target.result);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                setSignature(null);
+                // alert('Пожалуйста, загрузите изображение в формате PNG.');
+            }
+        }
+    };
+
+    const handleStampUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            if (file.type === 'image/png') {
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    setStamp(event.target.result);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                setStamp(null);
+                // alert('Пожалуйста, загрузите изображение в формате PNG.');
+            }
+        }
+    };
+
+    const handleSignatureDrag = (e, data) => {
+        setSignaturePosition({x: data.x, y: data.y});
+    };
+
+    const handleStampDrag = (e, data) => {
+        setStampPosition({x: data.x, y: data.y});
+    };
+
+    const handleCreateJson = () => {
+        // Создание JSON объекта
+        const jsonToSave = {
+            text_field: textBlocks.map((block) => ({
+                text: block.text,
+                x: block.x,
+                y: block.y,
+                fontFamily: block.fontFamily,
+                fontSize: block.fontSize,
+                italic: block.isItalic,
+                textDecoration: block.isDecoration,
+                fontWeight: block.isBold,
+            })),
+            background: {
+                width: 600,
+                height: 850,
+            }, // Подставьте URL фона
+            Stamp: {
+                url: stamp,
+                x: stampPosition.x,
+                y: stampPosition.y,
+            },
+            Signature: {
+                url: signature,
+                x: signaturePosition.x,
+                y: signaturePosition.y,
+            }, // Подставьте URL печати
+        };
+        // console.log(jsonToSave)
+        setPdfData(jsonToSave);
+    };
+
+    const handleSavePDF = async () => {
+        handleCreateJson();
+        const scale = 3; // Увеличение разрешения в 3 раза
+        const canvas = await html2canvas(certificateRef.current, {scale});
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new JsPDF();
+        pdf.addImage(imgData, 'PNG', 0, 0, 210, 300, '', 'FAST');
+        pdf.save('certificate.pdf');
+    };
+
+    const handleCertificateUpload = (uploadedImage) => {
+        const img = new Image();
+        img.src = uploadedImage;
+        img.onload = () => {
+            if (img.width === 600 && img.height === 850) {
+                setUploadedCertificate(uploadedImage);
+            } else {
+                // alert(
+                // 	'Загруженная грамота должна быть размером 600x850 пикселей. Загрузка отменена.'
+                // );
+            }
+        };
+    };
+
     return (
         <main className="main-content-editor">
-            <SidebarEditor />
-            <CertificateEditor />
+            <SidebarEditor
+                textPanelActive={textPanelActive}
+            />
+            <CertificateEditor
+                setEditingTextIndex={setEditingTextIndex}
+                editingTextIndex={editingTextIndex}
+                font={font}
+                setFont={setFont}
+                fontSize={fontSize}
+                setFontSize={setFontSize}
+                onFontChange={handleFontChange}
+                onFontSizeChange={handleFontSizeChange}
+                textBlocks={textBlocks}
+                setTextBlocks={setTextBlocks}
+                certificateRef={certificateRef}
+                isVisible={showProperties}
+                setActiveTextIndex={setActiveTextIndex}
+                activeTextIndex={activeTextIndex}
+                setShowProperties={setShowProperties}
+                setTextDecorationStyle={setTextDecorationStyle}
+                setTextPosition={setTextPosition}
+                onTextClick={handleTextClick}
+                textBlockStyles={textBlockStyles}
+                setTextBlockStyles={setTextBlockStyles}
+                setTextAlignStyle={setTextAlignStyle}
+                handleTextChange={handleTextChange}
+                handleInputKeyDown={handleInputKeyDown}
+                onSignatureUpload={handleSignatureUpload}
+                onSavePDF={handleSavePDF}
+                onCertificateUpload={handleCertificateUpload}
+                showTable={showTable}
+                setShowTable={setShowTable}
+                tableData={tableData}
+                setTableData={setTableData}
+                onStampUpload={handleStampUpload}
+                onCreateJson={handleCreateJson}
+                uploadedCertificate={uploadedCertificate}
+                signature={signature}
+                signaturePosition={signaturePosition}
+                onSignatureDrag={handleSignatureDrag}
+                stamp={stamp}
+                stampPosition={stampPosition}
+                onStampDrag={handleStampDrag}
+            />
         </main>
     );
 }
--- src/components/PageEditor/SidebarEditor/TextPanel/TextPanel.jsx
+++ src/components/PageEditor/SidebarEditor/TextPanel/TextPanel.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import downloadIcon from '../../../../images/imageEditor/download-icon.png';
 
-function TextPanel(props) {
+function TextPanel({
+                       textPanelActive
+                   }) {
+
+    const [btnClick, setBtnClick] = useState(true);
+
+    const onClickBtnActive = () => {
+        setBtnClick(false);
+    }
+
+    const onClickBtnNotActive = () => {
+        setBtnClick(true);
+    }
+
     return (
         <div className="text-panel">
             <div className="text-panel__block-button">
-                <button className="text-panel_button">Текст</button>
-                <button className="text-panel_button">Мои шрифты</button>
-            </div>
-            <div className="text-panel__block-result">
-                <button className="text-panel__h1">Создать заголовок</button>
-                <button className="text-panel__h2">Создать заголовок</button>
-                <button className="text-panel__paragraph">Создать заголовок</button>
-            </div>
-            <div className="text-panel__block-result">
-                <button className="text-panel__download-font">
-                    <img src={downloadIcon} alt="" className="text-panel__download-icon"/>
-                    Создать заголовок
+                <button
+                    className={`text-panel__button ${btnClick ? "text-panel__button_active" : ""}`}
+                    onClick={onClickBtnNotActive}
+                >
+                    Текст
+                </button>
+                <button
+                    className={`text-panel__button ${!btnClick ? "text-panel__button_active" : ""}`}
+                    onClick={onClickBtnActive}>Мои шрифты
                 </button>
             </div>
+            {btnClick ?
+                <div className="text-panel__block-result">
+                    <button className="text-panel__h1">Создать заголовок</button>
+                    <button className="text-panel__h2">Создать подзаголовок</button>
+                    <button className="text-panel__paragraph">Создать основной текст</button>
+                </div>
+                :
+                <div className="text-panel__block-download">
+                    <button className="text-panel__download-font">
+                        <img src={downloadIcon} alt="" className="text-panel__download-icon"/>
+                        Загрузить шрифт
+                    </button>
+                </div>
+            }
         </div>
     );
 }
