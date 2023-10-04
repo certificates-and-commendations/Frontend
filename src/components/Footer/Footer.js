2b7c19a6a6abaa6e77f27a1fc437e05420d0cd96
Yulia Markowa <markowayulia@yandex.ru>
2023-10-04 22:51:21 +0300

    new footer/buttons of header/icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,53 @@
-import Logo from '../Logo/Logo';
 import './Footer.css';
-import Telegram from '../../images/Telegram - Negative.svg';
+import Logo from '../Logo/Logo';
+import Telegram from '../../images/telegram.svg';
+import Twitter from '../../images/twitter.svg';
+import Facebook from '../../images/facebook.svg';
 
 export default function Footer() {
 	return (
-		<footer className="footer">
-			<div className="footer__column">
-				<Logo />
-				<div className="footer__info">
-					<p className="footer__text footer__text_margin_bottom">
-						© 2023 IT сообщество
-					</p>
-					<p className="footer__text">Напишите нам</p>
-					<p className="footer__text">Обратная связь и предложения</p>
-				</div>
+	  <footer className="footer">
+		<div className="footer__container">
+		  <div className="footer__column">
+			<Logo />
+			<div className="footer__info">
+			  <p className="footer__text">&#169;2023</p>
+			  <p className="footer__text">IT сообщество</p>
 			</div>
-			<a className="footer__link" href="p" target="_blank">
+		  </div>
+		  <ul className="footer__list">
+			<li className="footer__item">
+			  <a className="footer__link" href="p" target="_blank">
 				<img
-					className="footer__link-img"
-					src={Telegram}
-					alt="Логотип телеграмма"
+				  className="footer__link-img"
+				  src={Telegram}
+				  alt="Логотип телеграма"
 				/>
-			</a>
-		</footer>
+				<span className="footer__link-text">Telegram</span>
+			  </a>
+			</li>
+			<li className="footer__item">
+			  <a className="footer__link" href="p" target="_blank">
+				<img
+				  className="footer__link-img"
+				  src={Twitter}
+				  alt="Логотип телеграма"
+				/>
+				<span className="footer__link-text">Twitter</span>
+			  </a>
+			</li>
+			<li className="footer__item">
+			  <a className="footer__link" href="p" target="_blank">
+				<img
+				  className="footer__link-img"
+				  src={Facebook}
+				  alt="Логотип телеграма"
+				/>
+				<span className="footer__link-text">Facebook</span>
+			  </a>
+			</li>
+		  </ul>
+		</div>
+	  </footer>
 	);
-}
+  }
